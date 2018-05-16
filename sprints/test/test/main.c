5c2a4483dfafc87fcf537f1155926b876be759b2
robinovitch61 <leorobinovitch@gmail.com>
2018-05-16 15:13:32 -0400

    start summary

---
+++ sprints/test/test/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  test
+//
+//  Created by Leo Robinovitch on 2018-05-15.
+//  Copyright © 2018 Leo Robinovitch. All rights reserved.
+//
+
+#include <stdio.h>
+int main()
+{
+    printf( "I am alive!  Beware.\n" );
+    char test;
+    test = getchar();
+    printf("%c", test);
+    return 0;
+}
